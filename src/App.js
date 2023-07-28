18f5d34c8b5c5e441933bda98bac9fe30c15a399
chankxow <101338054+chankxow@users.noreply.github.com>
2023-07-28 19:19:51 +0700

    x

--- src/App.js
+++ src/App.js
@@ -1,32 +1,31 @@
-import React from 'react';
+import React from "react";
 // components
-import Banner from './components/Banner';
-import Nav from './components/Nav';
-import About from './components/About';
-import Project from './components/Project';
-import Activity from './components/Activity';
-import Contact from './components/Contact';
-import Certificate from './components/Certificate';
+import Banner from "./components/Banner";
+import Nav from "./components/Nav";
+import About from "./components/About";
+import Project from "./components/Project";
+import Activity from "./components/Activity";
+import Contact from "./components/Contact";
+import Certificate from "./components/Certificate";
 const App = () => {
   return (
-    <div className='bg-ac bgAnimation bg-no-repeat overflow-hidden bg-cover'>
-    <Banner/>
-    <div>
-    <div className=''>
-      <About/>
-      <div><Project/></div>
-      
-      <Activity/>
-      <Certificate/>
-      <Contact/>
-      <Nav/>
-      <div className='h-[4000px]'>
+    <div className="bg-ac bgAnimation bg-no-repeat overflow-hidden bg-cover">
+      <Banner />
+      <div>
+        <div className="">
+          <About />
+          <div>
+            <Project />
+          </div>
 
+          <Activity />
+          <Certificate />
+          <Contact />
+          <Nav />
+          <div className="h-[4000px]"></div>
+        </div>
       </div>
-      </div>
-      
-    </div></div>
-    
+    </div>
   );
 };
 
