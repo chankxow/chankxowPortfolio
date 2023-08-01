08eec9b9934796adad8747bd79b962b8c0d15dee
chankxow <101338054+chankxow@users.noreply.github.com>
2023-08-01 17:38:01 +0700

    x

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -11,18 +11,21 @@ const Nav = () => {
   return <nav className='fixed bottom-2 lg:bottom-8 w-full overflow-hidden z-50'>
   <div className="container mx-auto">
     {/*nav inner*/}
-    <div className='w-full bg-black/20 h-[96px] backdrop-blur-2x1 
-    rounded-full max-w-[460px] mx-auto px-5 flex justify-between items-center 
+    <div className='w-full bg-black/20 h-[70px] backdrop-blur-2x1 
+    rounded-full max-w-[400px] mx-auto px-5 flex justify-between items-center 
     text-2x1 text-white/50'>
-
-      <Link to='home' 
+   <div><Link to='home' 
       activeClass='active' 
       smooth={true} 
       spy={true} 
       className='hoverX cursor-pointer h-[60px] w-[60px] mx-auto flex items-center justify-center '> 
          <BiHomeAlt/>
       </Link>
-
+      <span class="absolute -top-8 left-[50%] -translate-x-[50%] z-20 origin-left scale-0 rounded-lg border border-gray-300 bg-white px-3 py-2 text-sm font-medium shadow-md transition-all duration-300 ease-in-out group-hover:scale-100">
+    Home
+  </span>
+      </div>
+      
       <Link to='about' 
       activeClass='active' 
       smooth={true} 
