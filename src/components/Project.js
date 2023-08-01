0a486f7abdc34f8198004bcec6a53dbeda424bab
chankxow <101338054+chankxow@users.noreply.github.com>
2023-08-01 20:58:59 +0700

    Update Project.js

--- src/components/Project.js
+++ src/components/Project.js
@@ -9,9 +9,10 @@ const Project = () => {
       <div className="card relative w-[350px] aspect-video 
       rounded-xl flex items-center justify-center 
       overflow-hidden shadow-sm shadow-gray-950 
-      transition-all duration-500 hover:scale-105 hover:shadow-lg shadow-black">
+      transition-all duration-500 hover:scale-105 hover:shadow-lg hover:shadow-black">
   <img src={s} alt="" />
-  <div className="card__content">
+  <div className="card__content absolute top-0 left-0 w-max h-max padding-[20px]
+   box-border bg-[#f2f2f2] transition-all opacity-0 duration-[0.6s] hover:opacity-100">
     <p className="card__title">Project Name</p>
     <p className="card__description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
     <button className="card__button">Live Demo</button>
