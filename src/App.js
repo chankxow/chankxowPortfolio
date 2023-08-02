b1c62ab225c02c5c99f11138ee9b011330f32851
chankxow <101338054+chankxow@users.noreply.github.com>
2023-08-02 21:12:07 +0700

    x

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import About from './components/About';
 import Project from './components/Project';
 import Activity from './components/Activity';
 import Certificate from './components/Certificate';
+
 const App = () => {
   return (
     <div className='grid bg-ac bgAnimation bg-no-repeat overflow-hidden bg-cover'>
@@ -17,7 +18,7 @@ const App = () => {
       <Certificate/>
       <Nav/>
       <div className='h-[4000px]'>
-
+      
       </div>
       </div>
       </div>
--- src/components/Activity.js
+++ src/components/Activity.js
@@ -3,8 +3,11 @@ import { motion } from "framer-motion";
 import { fadeIn } from "../variants";
 const Activity = () => {
   return (
-    <div id="activity" className="text-black section max-w-max mx-h-max">
-      <div className="container mx-auto">
+    <div
+      id="activity"
+      className="text-black section max-w-max mx-h-max mx-auto"
+    >
+      <div className="container  text-center">
         <motion.h1
           variants={fadeIn("down", 0.2)}
           initial="hidden"
@@ -16,6 +19,44 @@ const Activity = () => {
         >
           -MY ACTIVITY-
         </motion.h1>
+
+        <div className=" max-h-max max-w-max ">
+          <div className="cards  ">
+            <div className="card red h-max">
+              <div>
+                <p className="tip">Hover Me</p>
+              </div>
+              <div className="second-text">
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  Suscipit accusamus distinctio eum tempora accusantium
+                  perspiciatis et similique, incidunt quibusdam velit?</p>
+              </div>
+            </div>
+            <div className="card white h-max ">
+              <div>
+                <p className="tip">Hover Me</p>
+              </div>
+              <div className="second-text ">
+                <p>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  Suscipit accusamus distinctio eum tempora accusantium
+                  perspiciatis et similique, incidunt quibusdam velit?
+                </p>
+              </div>
+            </div>
+            <div className="card blue h-max">
+              <div>
+                <p className="tip">Hover Me</p>
+              </div>
+
+              <div className="second-text">
+                <p className="">Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  Suscipit accusamus distinctio eum tempora accusantium
+                  perspiciatis et similique, incidunt quibusdam velit?</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
