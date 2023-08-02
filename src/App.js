0c00ccbffd0edf421f2272807561259a35803f72
chankxow <101338054+chankxow@users.noreply.github.com>
2023-08-03 00:03:55 +0700

    x

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
-import React from 'react';
+import React from "react";
 // components
-import Banner from './components/Banner';
-import Nav from './components/Nav';
-import About from './components/About';
-import Project from './components/Project';
-import Activity from './components/Activity';
-import Certificate from './components/Certificate';
-
+import Banner from "./components/Banner";
+import Nav from "./components/Nav";
+import About from "./components/About";
+import Project from "./components/Project";
+import Activity from "./components/Activity";
+import Certificate from "./components/Certificate";
 const App = () => {
   return (
-    <div className='grid bg-ac bgAnimation bg-no-repeat overflow-hidden bg-cover'>
-    <Banner/>
-    <div>
-      <About/>
-      <Project/>
-      <Activity/>
-      <Certificate/>
-      <Nav/>
-      <div className='h-[4000px]'>
-      
+    <div className="grid bg-ac bgAnimation bg-no-repeat overflow-hidden bg-cover">
+      <Banner />
+      <div>
+        <About />
+        <Project />
+        <Activity />
+        <Certificate />
+        <Nav />
+        <div className="h-[4000px]"></div>
       </div>
-      </div>
-      </div>
+    </div>
   );
 };
 
