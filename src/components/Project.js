7fe787da21ed454195a8c20a200fde401864edaf
chankxow <101338054+chankxow@users.noreply.github.com>
2023-08-02 12:41:37 +0700

    x

--- src/components/Project.js
+++ src/components/Project.js
@@ -22,12 +22,48 @@ const Project = () => {
       rounded-xl flex items-center justify-center 
       overflow-hidden shadow-sm shadow-gray-950 
       transition-all duration-500 hover:scale-105 hover:shadow-lg hover:shadow-black ">
-      <h1 className="text-2xl font-bold text-black shadow-md shadow-black m-auto bg-white/30">SMARTHYDROPONIC</h1>
+      <h1 className="text-2xl font-bold text-blue-700 shadow-md shadow-black m-auto bg-white/80">SMART HYDROPONIC</h1>
+      
+  <div className=" card__content absolute top-0 left-0 h-[100%] w-[100%] padding-[20px]
+   box-border bg-white/90 transition-all opacity-0 duration-[0.3s] hover:opacity-100">
+    <p className="card__title m-5 text-black font-bold">SMARTHYDROPONIC</p>
+    <p className="card__description m-5 text-black leading-6">เป็นโครงงานแข่งขันโดยจะนำการใช้ระบบ IOT มาใช้ในทางการเกษตรร่วมกับการปลูกพืชแบบไฮโดรโปนิค</p>
+    <a className='p-[5px] mx-auto relative text-gray-400 bg-slate-600 fill-currents w-max 
+                    flex justify-center items-center rounded transition-all duration-700 hover:fill-current
+                     hover:text-white hover:bg-gradient-to-tr from-violet-800 pink-500 to-red-500' href="https://github.com/chankxow/Project_matthayom_5">
+                      READ MORE
+                    </a>
+  </div>
+</div>
+
+<div className=" bg-cover card mr-[20px] relative w-[300px] h-[250px] aspect-video 
+      rounded-xl flex items-center justify-center 
+      overflow-hidden shadow-sm shadow-gray-950 
+      transition-all duration-500 hover:scale-105 hover:shadow-lg hover:shadow-black ">
+      <h1 className="text-2xl font-bold text-black shadow-md shadow-black m-auto bg-white/30">FOR PROJECT</h1>
       
   <div className=" card__content absolute top-0 left-0 h-[100%] w-[100%] padding-[20px]
    box-border bg-white/80 transition-all opacity-0 duration-[0.3s] hover:opacity-100">
-    <p className="card__title m-5 text-black font-bold">SMARTHYDROPONIC</p>
-    <p className="card__description m-5 text-black leading-6">เป็นโครงงานแข่งขันโดยจะนำการใช้ระบบ IOT มาใช้ในทางการเกษตรร่วมกับการปลูกพืชแบบไฮโดรโปนิค</p>
+    <p className="card__title m-5 text-black font-bold">FOR PROJECT</p>
+    <p className="card__description m-5 text-black leading-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, velit?</p>
+    <a className='p-[5px] mx-auto relative text-gray-400 bg-slate-600 fill-currents w-max 
+                    flex justify-center items-center rounded transition-all duration-700 hover:fill-current
+                     hover:text-white hover:bg-gradient-to-tr from-violet-800 pink-500 to-red-500' href="https://github.com/chankxow/Project_matthayom_5">
+                      READ MORE
+                    </a>
+  </div>
+</div>
+
+<div className=" bg-cover card mr-[20px] relative w-[300px] h-[250px] aspect-video 
+      rounded-xl flex items-center justify-center 
+      overflow-hidden shadow-sm shadow-gray-950 
+      transition-all duration-500 hover:scale-105 hover:shadow-lg hover:shadow-black ">
+      <h1 className="text-2xl font-bold text-black shadow-md shadow-black m-auto bg-white/30">FOR PROJECT</h1>
+      
+  <div className=" card__content absolute top-0 left-0 h-[100%] w-[100%] padding-[20px]
+   box-border bg-white/80 transition-all opacity-0 duration-[0.3s] hover:opacity-100">
+    <p className="card__title m-5 text-black font-bold">FOR PROJECT</p>
+    <p className="card__description m-5 text-black leading-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, dolores.</p>
     <a className='p-[5px] mx-auto relative text-gray-400 bg-slate-600 fill-currents w-max 
                     flex justify-center items-center rounded transition-all duration-700 hover:fill-current
                      hover:text-white hover:bg-gradient-to-tr from-violet-800 pink-500 to-red-500' href="https://github.com/chankxow/Project_matthayom_5">
