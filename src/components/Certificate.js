fa5983da0ed2b2d7d7e05e49119eda5d3858a427
chankxow <101338054+chankxow@users.noreply.github.com>
2023-08-03 22:08:04 +0700

    Update Certificate.js

--- src/components/Certificate.js
+++ src/components/Certificate.js
@@ -1,7 +1,13 @@
-import React from 'react';
-
+import React from "react";
+import { motion } from "framer-motion";
+import { fadeIn } from "../variants";
 const Certificate = () => {
-  return <div id='certificate' className='section text-black'>Certificate</div>;
+  return (
+    <div id="certificate" className="section text-black">
+    <div className="container"></div>
+      Certificate
+    </div>
+  );
 };
 
 export default Certificate;
