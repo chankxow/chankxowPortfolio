5b2707e9eaa86f15357f25c136db58fd32388697
chankxow <101338054+chankxow@users.noreply.github.com>
2023-08-05 16:25:37 +0700

    x

--- src/components/Popupimg.js
+++ src/components/Popupimg.js
@@ -1,10 +1,28 @@
-import React from 'react';
-const Popup = () => {
+import React, { useState } from 'react';
+import Modal from 'react-modal';
+import SchoolLogo from "../assets/about/stp-logo.png";
+
+function ImageModal() {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+  
+    const toggleModal = () => {
+      setIsModalOpen(!isModalOpen);
+    };
+  
     return (
-        <div ></div>
-       
-    )
-
-    }
-
-    export default Popup ;
+      <div>
+        <button onClick={toggleModal}>Open Modal</button>
+        <Modal
+          isOpen={isModalOpen}
+          onRequestClose={toggleModal}
+          contentLabel="Image Modal"
+        >
+          <img src={SchoolLogo} alt="Modal" />
+          <button onClick={toggleModal}>Close Modal</button>
+        </Modal>
+      </div>
+    );
+  }
+  
+  export default ImageModal;
+  
