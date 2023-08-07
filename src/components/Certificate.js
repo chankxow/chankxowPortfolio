73f2c4cf5b0ff616ea1a5d0ae6b6f41be300363b
chankxow <101338054+chankxow@users.noreply.github.com>
2023-08-07 23:06:19 +0700

    Update Certificate.js

--- src/components/Certificate.js
+++ src/components/Certificate.js
@@ -18,32 +18,103 @@ const Certificate = () => {
     font-bold text-transparent bg-gradient-to-r bg-clip-text
      from-blue-500 to-green-500 py-10  "
         >
-          -CERTIFICATE-
+          CERTIFICATE
         </motion.h1>
+        <div className="flex flex-col gap-10 lg:flex-row  ">
         <div className="bg-white h-full w-full ">
-        <div className="p-2 mx-auto">
-        <div class="max-w-sm rounded overflow-hidden shadow-lg">
-          <div className="">
-          <div
-              class=" bg-CESCaCER bg-cover h-[250px]  w-[350px]"
-              alt="Sunset in the mountains"
-            ></div></div>
-            
-            <div class="px-6 ">
-              <div class="font-bold text-xl mb-2">CESCa#17</div>
+            <div className="p-2 mx-auto ">
+              <div class="max-w-sm rounded overflow-hidden shadow-lg">
+                <div className="">
+                  <div
+                    class=" bg-CESCaCER bg-cover h-[250px]  w-[350px]"
+                    alt="Sunset in the mountains"
+                  ></div>
+                </div>
+
+                <div class="px-6 ">
+                  <div class="font-bold text-xl mb-2">CESCa#17</div>
+                </div>
+                <div class="px-6  pb-2">
+                  <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                    #CAMP
+                  </span>
+                  <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                    #ENGINEERING
+                  </span>
+                  <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                    #ARDUINO
+                  </span>
+                  <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                    #PROGRAMMING
+                  </span>
+                </div>
+              </div>
             </div>
-            <div class="px-6  pb-2">
-              <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                #CAMP
-              </span>
-              <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                #travel
-              </span>
-              <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                #winter
-              </span>
+          </div>
+
+          <div className="bg-white h-full w-full ">
+            <div className="p-2 mx-auto ">
+              <div class="max-w-sm rounded overflow-hidden shadow-lg">
+                <div className="">
+                  <div
+                    class=" bg-CESCaCER bg-cover h-[250px]  w-[350px]"
+                    alt="Sunset in the mountains"
+                  ></div>
+                </div>
+
+                <div class="px-6 ">
+                  <div class="font-bold text-xl mb-2">CESCa#17</div>
+                </div>
+                <div class="px-6  pb-2">
+                  <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                    #CAMP
+                  </span>
+                  <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                    #ENGINEERING
+                  </span>
+                  <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                    #ARDUINO
+                  </span>
+                  <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                    #PROGRAMMING
+                  </span>
+                </div>
+              </div>
             </div>
-          </div></div>
+          </div>
+
+          <div className="bg-white h-full w-full ">
+            <div className="p-2 mx-auto ">
+              <div class="max-w-sm rounded overflow-hidden shadow-lg">
+                <div className="">
+                  <div
+                    class=" bg-CESCaCER bg-cover h-[250px]  w-[350px]"
+                    alt="Sunset in the mountains"
+                  ></div>
+                </div>
+
+                <div class="px-6 ">
+                  <div class="font-bold text-xl mb-2">CESCa#17</div>
+                </div>
+                <div class="px-6  pb-2">
+                  <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                    #CAMP
+                  </span>
+                  <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                    #ENGINEERING
+                  </span>
+                  <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                    #ARDUINO
+                  </span>
+                  <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                    #PROGRAMMING
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          
           
         </div>
       </div>
