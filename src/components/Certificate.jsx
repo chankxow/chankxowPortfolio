130ff9d9f15150c3a3270283a3865600fdf96d73
chankxow <101338054+chankxow@users.noreply.github.com>
2023-08-22 12:32:16 +0700

    x

--- src/components/Certificate.jsx
+++ src/components/Certificate.jsx
@@ -1,4 +1,3 @@
-
 import { motion } from "framer-motion";
 import { fadeIn } from "../variants";
 
@@ -19,12 +18,14 @@ const Certificate = () => {
      from-blue-500 to-green-500 py-10  "
         >
           CERTIFICATE
-        </motion.h1 >
-        <motion.div 
-        variants={fadeIn("up", 0.3)}
+        </motion.h1>
+        <motion.div
+          variants={fadeIn("up", 0.3)}
           initial="hidden"
           whileInView={"show"}
-          viewport={{ once: true, amount: 0.7 }} className="flex flex-col gap-10 lg:flex-row  ">
+          viewport={{ once: true, amount: 0.7 }}
+          className="flex flex-col gap-10 lg:flex-row  "
+        >
           <div className="bg-white h-full w-full ">
             <div className="p-2 mx-auto ">
               <div className="max-w-sm rounded overflow-hidden shadow-lg">
@@ -67,7 +68,41 @@ const Certificate = () => {
                 </div>
 
                 <div className="px-6 ">
-                  <div className="font-bold text-base mb-2">โครงงานคอมพิวเตอร์ประเภทซอฟแวร์ ครั้งที่70</div>
+                  <div className="font-bold text-base mb-2">
+                    โครงงานคอมพิวเตอร์ประเภทซอฟแวร์ ครั้งที่70
+                  </div>
+                </div>
+                <div className="px-6  pb-2">
+                  <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                    #CAMP
+                  </span>
+                  <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                    #ENGINEERING
+                  </span>
+                  <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                    #ARDUINO
+                  </span>
+                  <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                    #PROGRAMMING
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="bg-white h-full w-full ">
+            <div className="p-2 mx-auto ">
+              <div className="max-w-sm rounded overflow-hidden shadow-lg">
+                <div className="">
+                  <div
+                    className=" bg-cdg bg-cover h-[250px]  w-[350px]"
+                    title="CERTIFICATE"
+                  ></div>
+                </div>
+
+                <div className="px-6 ">
+                  <div className="font-bold text-base mb-2">
+                  หลักสูตรอบรม Python Programming
+                  </div>
                 </div>
                 <div className="px-6  pb-2">
                   <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
